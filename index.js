0fb5ba37ed28fdf06deb5072a6f7f11e07e5b40d
Danilo Viacava <daniloviacava@hotmail.com>
2019-08-31 14:47:09 -0500

    models, schemas and resolvers in mongoose and graphql

--- index.js
+++ index.js
@@ -2,35 +2,73 @@ const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 import { makeExecutableSchema } from 'graphql-tools';
 import mongoose from 'mongoose';
-moongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 // import typeDefs from './schemas'
 // import resolvers from './resolvers'
+import models from './models'
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+// const typeDefs = gql`
+//   type User {
+//     _id: ID!
+//     username: String!
+//     password: String!
+//   }
+
+//   type Query {
+//     allUsers: [User]!
+//     getUser(_id: ID!): User!
+//   }
+
+//   type Mutation {
+//     createUser(username: String!, password: String!): User!
+//   }
+// `;
 
 // Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Mi primer query de Graphql!',
-  },
-};
+// const resolvers = {
+//   Query: {
+//     allUsers: (parent, args, {models}) => models.User.find(),
+//     getUser: (parent, args, {models}) => models.User.findOne(args),
+//   },
+//   Mutation: {
+//     createUser: (parent, args, {models}) => models.User.create(args)
+//   }
+  
+// };
+
+//mezclar todos los archivos de carpetas de types y resolvers
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './types')));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers/')));
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 })
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ 
+  schema,
+  context: {
+    models,
+    user: {
+      _id: 1, username: 'Juan'
+    }
+  } 
+});
 
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+mongoose.connect('mongodb://localhost:27017/carrito', {useNewUrlParser: true}).then(
+  () => {
+    console.log('Conectado a Mongo!')
+    app.listen({ port: 4001 }, () =>
+      console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`)
+    );
+  }
+)
+
+
---
+++ resolvers/users.js
@@ -0,0 +1,9 @@
+export default {
+    Query: {
+        allUsers: (parent, args, {models}) => models.User.find(),
+        getUser: (parent, args, {models}) => models.User.findOne(args),
+    },
+    Mutation: {
+        createUser: (parent, args, {models}) => models.User.create(args)
+    }
+}
